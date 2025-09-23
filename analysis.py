a7ce4dc21a40adb9cbffd91b4e9b5c320c0da655
IHyperLight <blacky.0412@hotmail.com>
2025-09-23 16:10:36 -0600

    f

--- analysis.py
+++ analysis.py
@@ -8,92 +8,135 @@ lexical_error_occurred = False
 syntax_error_occurred = False
 
 tokens = (
-    'CREATE', 'DATABASE', 'USE', 'TABLE', 'INSERT', 'INTO', 'UPDATE', 'DELETE',
-    'ID', 'COMMA', 'SEMICOLON', 'LPAREN', 'RPAREN', 'ASTERISK', 'SET', 'VALUES',
-    'WHERE', 'AND', 'OR', 'NOT', 'NULL', 'PRIMARY', 'KEY', 'FOREIGN', 'REFERENCES',
-    'INT', 'FLOAT', 'VARCHAR', 'TEXT', 'DATETIME', 'TIMESTAMP', 'DATE', 'TIME',
-    'UNSIGNED', 'AUTO_INCREMENT', 'EQUALS', 'PLUS', 'MINUS', 'DIVIDE', 'MOD',
-    'GREATER', 'LESS', 'GREATER_EQ', 'LESS_EQ', 'NOT_EQ', 'LIKE', 'STRING', 'FROM', 'NUMBER',
+    "CREATE",
+    "DATABASE",
+    "USE",
+    "TABLE",
+    "INSERT",
+    "INTO",
+    "UPDATE",
+    "DELETE",
+    "ID",
+    "COMMA",
+    "SEMICOLON",
+    "LPAREN",
+    "RPAREN",
+    "ASTERISK",
+    "SET",
+    "VALUES",
+    "WHERE",
+    "AND",
+    "OR",
+    "NOT",
+    "NULL",
+    "PRIMARY",
+    "KEY",
+    "FOREIGN",
+    "REFERENCES",
+    "INT",
+    "FLOAT",
+    "VARCHAR",
+    "TEXT",
+    "DATETIME",
+    "TIMESTAMP",
+    "DATE",
+    "TIME",
+    "UNSIGNED",
+    "AUTO_INCREMENT",
+    "EQUALS",
+    "PLUS",
+    "MINUS",
+    "DIVIDE",
+    "MOD",
+    "GREATER",
+    "LESS",
+    "GREATER_EQ",
+    "LESS_EQ",
+    "NOT_EQ",
+    "LIKE",
+    "STRING",
+    "FROM",
+    "NUMBER",
 )
 
 reserved = {
-    'create': 'CREATE',
-    'database': 'DATABASE',
-    'use': 'USE',
-    'table': 'TABLE',
-    'insert': 'INSERT',
-    'into': 'INTO',
-    'update': 'UPDATE',
-    'delete': 'DELETE',
-    'set': 'SET',
-    'values': 'VALUES',
-    'where': 'WHERE',
-    'and': 'AND',
-    'or': 'OR',
-    'not': 'NOT',
-    'null': 'NULL',
-    'primary': 'PRIMARY',
-    'key': 'KEY',
-    'foreign': 'FOREIGN',
-    'references': 'REFERENCES',
-    'int': 'INT',
-    'float': 'FLOAT',
-    'varchar': 'VARCHAR',
-    'text': 'TEXT',
-    'datetime': 'DATETIME',
-    'timestamp': 'TIMESTAMP',
-    'date': 'DATE',
-    'time': 'TIME',
-    'unsigned': 'UNSIGNED',
-    'auto_increment': 'AUTO_INCREMENT',
-    'equals': 'EQUALS',
-    'plus': 'PLUS',
-    'minus': 'MINUS',
-    'divide': 'DIVIDE',
-    'mod': 'MOD',
-    'greater': 'GREATER',
-    'less': 'LESS',
-    'greater_eq': 'GREATER_EQ',
-    'less_eq': 'LESS_EQ',
-    'not_eq': 'NOT_EQ',
-    'like': 'LIKE',
-    'from': 'FROM',
+    "create": "CREATE",
+    "database": "DATABASE",
+    "use": "USE",
+    "table": "TABLE",
+    "insert": "INSERT",
+    "into": "INTO",
+    "update": "UPDATE",
+    "delete": "DELETE",
+    "set": "SET",
+    "values": "VALUES",
+    "where": "WHERE",
+    "and": "AND",
+    "or": "OR",
+    "not": "NOT",
+    "null": "NULL",
+    "primary": "PRIMARY",
+    "key": "KEY",
+    "foreign": "FOREIGN",
+    "references": "REFERENCES",
+    "int": "INT",
+    "float": "FLOAT",
+    "varchar": "VARCHAR",
+    "text": "TEXT",
+    "datetime": "DATETIME",
+    "timestamp": "TIMESTAMP",
+    "date": "DATE",
+    "time": "TIME",
+    "unsigned": "UNSIGNED",
+    "auto_increment": "AUTO_INCREMENT",
+    "equals": "EQUALS",
+    "plus": "PLUS",
+    "minus": "MINUS",
+    "divide": "DIVIDE",
+    "mod": "MOD",
+    "greater": "GREATER",
+    "less": "LESS",
+    "greater_eq": "GREATER_EQ",
+    "less_eq": "LESS_EQ",
+    "not_eq": "NOT_EQ",
+    "like": "LIKE",
+    "from": "FROM",
 }
 
 
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     return t
 
 
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value.lower(), 'ID')
+    r"[a-zA-Z_][a-zA-Z_0-9]*"
+    t.type = reserved.get(t.value.lower(), "ID")
     return t
 
 
-t_COMMA = r','
-t_SEMICOLON = r';'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_ASTERISK = r'\*'
-t_EQUALS = r'='
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_DIVIDE = r'/'
-t_MOD = r'%'
-t_GREATER = r'>'
-t_LESS = r'<'
-t_GREATER_EQ = r'>='
-t_LESS_EQ = r'<='
-t_NOT_EQ = r'<>|!='
-t_STRING = r'\'[^\']*\'|\"[^\"]*\"'
+t_COMMA = r","
+t_SEMICOLON = r";"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_ASTERISK = r"\*"
+t_EQUALS = r"="
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_DIVIDE = r"/"
+t_MOD = r"%"
+t_GREATER = r">"
+t_LESS = r"<"
+t_GREATER_EQ = r">="
+t_LESS_EQ = r"<="
+t_NOT_EQ = r"<>|!="
+t_STRING = r"\'[^\']*\'|\"[^\"]*\""
 
-t_ignore = ' \t\n'
+t_ignore = " \t\n"
 
 
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
 
 
@@ -109,38 +152,38 @@ lexer = lex.lex()
 
 
 def p_start(p):
-    '''start : create_database
-             | use
-             | create_table
-             | insert_into
-             | update
-             | delete
-    '''
+    """start : create_database
+    | use
+    | create_table
+    | insert_into
+    | update
+    | delete
+    """
     p[0] = p[1]
 
 
 def p_create_database(p):
-    '''create_database : CREATE DATABASE ID SEMICOLON
-                       | CREATE DATABASE ID'''
-    p[0] = {'action': 'create_database', 'name': p[3]}
+    """create_database : CREATE DATABASE ID SEMICOLON
+    | CREATE DATABASE ID"""
+    p[0] = {"action": "create_database", "name": p[3]}
 
 
 def p_use(p):
-    '''use : USE ID SEMICOLON
-           | USE ID'''
-    p[0] = {'action': 'use', 'name': p[2]}
+    """use : USE ID SEMICOLON
+    | USE ID"""
+    p[0] = {"action": "use", "name": p[2]}
 
 
 def p_create_table(p):
-    '''create_table : CREATE TABLE ID LPAREN column_definitions RPAREN SEMICOLON
-                    | CREATE TABLE ID LPAREN column_definitions RPAREN'''
-    p[0] = {'action': 'create_table', 'name': p[3], 'columns': p[5]}
+    """create_table : CREATE TABLE ID LPAREN column_definitions RPAREN SEMICOLON
+    | CREATE TABLE ID LPAREN column_definitions RPAREN"""
+    p[0] = {"action": "create_table", "name": p[3], "columns": p[5]}
 
 
 def p_column_definitions(p):
-    '''column_definitions : column_definition
-                          | column_definitions COMMA column_definition
-    '''
+    """column_definitions : column_definition
+    | column_definitions COMMA column_definition
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
@@ -148,31 +191,31 @@ def p_column_definitions(p):
 
 
 def p_column_definition(p):
-    'column_definition : ID data_type column_constraints'
-    p[0] = {'name': p[1], 'data_type': p[2], 'constraints': p[3]}
+    "column_definition : ID data_type column_constraints"
+    p[0] = {"name": p[1], "data_type": p[2], "constraints": p[3]}
 
 
 def p_data_type(p):
-    '''data_type : INT
-                 | FLOAT
-                 | VARCHAR LPAREN NUMBER RPAREN
-                 | TEXT
-                 | DATETIME
-                 | TIMESTAMP
-                 | DATE
-                 | TIME
-    '''
+    """data_type : INT
+    | FLOAT
+    | VARCHAR LPAREN NUMBER RPAREN
+    | TEXT
+    | DATETIME
+    | TIMESTAMP
+    | DATE
+    | TIME
+    """
     if len(p) == 2:
         p[0] = p[1]
     else:
-        p[0] = f'{p[1]}({p[3]})'
+        p[0] = f"{p[1]}({p[3]})"
 
 
 def p_column_constraints(p):
-    '''column_constraints : column_constraint
-                          | column_constraints column_constraint
-                          | empty
-    '''
+    """column_constraints : column_constraint
+    | column_constraints column_constraint
+    | empty
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     elif p[1] is None:
@@ -182,32 +225,31 @@ def p_column_constraints(p):
 
 
 def p_column_constraint(p):
-    '''column_constraint : NULL
-                         | NOT NULL
-                         | PRIMARY KEY
-                         | FOREIGN KEY REFERENCES ID
-                         | UNSIGNED
-                         | AUTO_INCREMENT
-    '''
+    """column_constraint : NULL
+    | NOT NULL
+    | PRIMARY KEY
+    | FOREIGN KEY REFERENCES ID
+    | UNSIGNED
+    | AUTO_INCREMENT
+    """
     if len(p) == 2:
         p[0] = p[1]
     elif len(p) == 3:
-        p[0] = f'{p[1]} {p[2]}'
+        p[0] = f"{p[1]} {p[2]}"
     else:
-        p[0] = f'{p[1]} {p[2]} {p[3]} {p[4]}'
+        p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]}"
 
 
 def p_insert_into(p):
-    '''insert_into : INSERT INTO ID LPAREN column_names RPAREN VALUES LPAREN values RPAREN SEMICOLON
-                   | INSERT INTO ID LPAREN column_names RPAREN VALUES LPAREN values RPAREN'''
-    p[0] = {'action': 'insert_into', 'table': p[3],
-            'columns': p[5], 'values': p[9]}
+    """insert_into : INSERT INTO ID LPAREN column_names RPAREN VALUES LPAREN values RPAREN SEMICOLON
+    | INSERT INTO ID LPAREN column_names RPAREN VALUES LPAREN values RPAREN"""
+    p[0] = {"action": "insert_into", "table": p[3], "columns": p[5], "values": p[9]}
 
 
 def p_column_names(p):
-    '''column_names : ID
-                    | column_names COMMA ID
-    '''
+    """column_names : ID
+    | column_names COMMA ID
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
@@ -215,9 +257,9 @@ def p_column_names(p):
 
 
 def p_values(p):
-    '''values : value
-              | values COMMA value
-    '''
+    """values : value
+    | values COMMA value
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
@@ -225,25 +267,29 @@ def p_values(p):
 
 
 def p_value(p):
-    '''value : INT
-             | FLOAT
-             | STRING
-             | NULL
-    '''
+    """value : INT
+    | FLOAT
+    | STRING
+    | NULL
+    """
     p[0] = p[1]
 
 
 def p_update(p):
-    '''update : UPDATE ID SET assignments WHERE where_conditions SEMICOLON
-              | UPDATE ID SET assignments WHERE where_conditions'''
-    p[0] = {'action': 'update', 'table': p[2],
-            'assignments': p[4], 'where_conditions': p[6]}
+    """update : UPDATE ID SET assignments WHERE where_conditions SEMICOLON
+    | UPDATE ID SET assignments WHERE where_conditions"""
+    p[0] = {
+        "action": "update",
+        "table": p[2],
+        "assignments": p[4],
+        "where_conditions": p[6],
+    }
 
 
 def p_assignments(p):
-    '''assignments : assignment
-                   | assignments COMMA assignment
-    '''
+    """assignments : assignment
+    | assignments COMMA assignment
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
@@ -251,54 +297,54 @@ def p_assignments(p):
 
 
 def p_assignment(p):
-    'assignment : ID EQUALS value'
-    p[0] = {'column': p[1], 'value': p[3]}
+    "assignment : ID EQUALS value"
+    p[0] = {"column": p[1], "value": p[3]}
 
 
 def p_delete(p):
-    '''delete : DELETE FROM ID WHERE where_conditions SEMICOLON
-              | DELETE FROM ID WHERE where_conditions'''
-    p[0] = {'action': 'delete', 'table': p[3], 'where_conditions': p[5]}
+    """delete : DELETE FROM ID WHERE where_conditions SEMICOLON
+    | DELETE FROM ID WHERE where_conditions"""
+    p[0] = {"action": "delete", "table": p[3], "where_conditions": p[5]}
 
 
 def p_where_conditions(p):
-    '''where_conditions : where_condition
-                        | where_conditions AND where_condition
-                        | where_conditions OR where_condition
-    '''
+    """where_conditions : where_condition
+    | where_conditions AND where_condition
+    | where_conditions OR where_condition
+    """
     if len(p) == 2:
         p[0] = [p[1]]
     else:
-        p[0] = p[1] + [{'operator': p[2], 'condition': p[3]}]
+        p[0] = p[1] + [{"operator": p[2], "condition": p[3]}]
 
 
 def p_where_condition(p):
-    '''where_condition : ID comparison_operator value
-                       | NOT where_condition
-                       | LPAREN where_conditions RPAREN
-    '''
+    """where_condition : ID comparison_operator value
+    | NOT where_condition
+    | LPAREN where_conditions RPAREN
+    """
     if len(p) == 2:
-        p[0] = {'not': p[1], 'condition': p[2]}
+        p[0] = {"not": p[1], "condition": p[2]}
     elif len(p) == 3:
         p[0] = p[1]
     else:
-        p[0] = {'column': p[1], 'operator': p[2], 'value': p[3]}
+        p[0] = {"column": p[1], "operator": p[2], "value": p[3]}
 
 
 def p_comparison_operator(p):
-    '''comparison_operator : EQUALS
-                           | NOT_EQ
-                           | GREATER
-                           | LESS
-                           | GREATER_EQ
-                           | LESS_EQ
-                           | LIKE
-    '''
+    """comparison_operator : EQUALS
+    | NOT_EQ
+    | GREATER
+    | LESS
+    | GREATER_EQ
+    | LESS_EQ
+    | LIKE
+    """
     p[0] = p[1]
 
 
 def p_empty(p):
-    'empty :'
+    "empty :"
     pass
 
 
@@ -308,8 +354,7 @@ def p_error(p):
         error_message = f"Syntax error at token: {p.type}, value: {p.value}, position: {p.lineno}, {p.lexpos}"
         syntax_errors.append(error_message)
     else:
-        syntax_errors.append(
-            "Syntax error, the structure of your sentence is invalid.")
+        syntax_errors.append("Syntax error, the structure of your sentence is invalid.")
     syntax_error_occurred = True
 
 
@@ -323,31 +368,55 @@ def semantic_analyzer(p, connection):
     if result is None:
         return False, "Error: Parsing error detected"
 
-    action = result['action']
+    action = result["action"]
     message = ""
 
-    if action == 'create_database':
-        success = not check_database_exists(result['name'], connection)
-        message = "Database created successfully" if success else "Error: Database already exists"
-    elif action == 'use':
-        success = check_database_exists(result['name'], connection)
+    if action == "create_database":
+        success = not check_database_exists(result["name"], connection)
+        message = (
+            "Database created successfully"
+            if success
+            else "Error: Database already exists"
+        )
+    elif action == "use":
+        success = check_database_exists(result["name"], connection)
         message = "Using database" if success else "Error: Database does not exist"
-    elif action == 'create_table':
-        success = check_database_exists(get_current_database(
-            connection), connection) and not check_table_exists(result['name'], connection)
-        message = "Table created successfully" if success else "Error: Table already exists or database does not exist"
-    elif action == 'insert_into':
-        success = check_database_exists(get_current_database(
-            connection), connection) and check_table_exists(result['table'], connection)
-        message = "Insert operation successful" if success else "Error: Table or database does not exist"
-    elif action == 'update':
-        success = check_database_exists(get_current_database(
-            connection), connection) and check_table_exists(result['table'], connection)
-        message = "Update operation successful" if success else "Error: Table or database does not exist"
-    elif action == 'delete':
-        success = check_database_exists(get_current_database(
-            connection), connection) and check_table_exists(result['table'], connection)
-        message = "Delete operation successful" if success else "Error: Table or database does not exist"
+    elif action == "create_table":
+        success = check_database_exists(
+            get_current_database(connection), connection
+        ) and not check_table_exists(result["name"], connection)
+        message = (
+            "Table created successfully"
+            if success
+            else "Error: Table already exists or database does not exist"
+        )
+    elif action == "insert_into":
+        success = check_database_exists(
+            get_current_database(connection), connection
+        ) and check_table_exists(result["table"], connection)
+        message = (
+            "Insert operation successful"
+            if success
+            else "Error: Table or database does not exist"
+        )
+    elif action == "update":
+        success = check_database_exists(
+            get_current_database(connection), connection
+        ) and check_table_exists(result["table"], connection)
+        message = (
+            "Update operation successful"
+            if success
+            else "Error: Table or database does not exist"
+        )
+    elif action == "delete":
+        success = check_database_exists(
+            get_current_database(connection), connection
+        ) and check_table_exists(result["table"], connection)
+        message = (
+            "Delete operation successful"
+            if success
+            else "Error: Table or database does not exist"
+        )
     else:
         return False, "Error: Invalid action"
 
@@ -415,6 +484,7 @@ def run_syntax_analyzer(query):
 def run_semantic_analyzer(connection, query):
     result, text = semantic_analyzer(query, connection)
     error_occurred = not result
-    result_string = "Semantic Result: " + \
-        ("Valid - " if result else "Invalid - ") + text
+    result_string = (
+        "Semantic Result: " + ("Valid - " if result else "Invalid - ") + text
+    )
     return result_string, error_occurred
--- app.py
+++ app.py
@@ -5,16 +5,12 @@ from analysis import run_lexical_analyzer, run_syntax_analyzer, run_semantic_ana
 
 app = Flask(__name__)
 
-app.secret_key = 'llave_secreta'
+app.secret_key = "llave_secreta"
 
 
 def create_mysql_connection(host, user, password):
     try:
-        connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password
-        )
+        connection = mysql.connector.connect(host=host, user=user, password=password)
         return connection
     except mysql.connector.Error as error:
         print(f"Error: {error}")
@@ -27,42 +23,46 @@ def execute_query(connection, query):
         cursor.execute(query)
         connection.commit()
         print("Query executed successfully")
-        result = 'Éxito, sentencias ejecutadas correctamente'
+        result = "Éxito, sentencias ejecutadas correctamente"
         return result
     except mysql.connector.Error as e:
         print(f"Error executing query: {e}")
         session["semant_result"] = str(e)
-        result = 'Error, revisa las salidas de los analizadores.'
+        result = "Error, revisa las salidas de los analizadores."
         return result
 
 
 def logic(connection, step):
     analyzers = [
-        ('lex', run_lexical_analyzer),
-        ('sintact', run_syntax_analyzer),
-        ('semant', run_semantic_analyzer),
+        ("lex", run_lexical_analyzer),
+        ("sintact", run_syntax_analyzer),
+        ("semant", run_semantic_analyzer),
     ]
     return_result = []
     flag = False
 
     results = {}
     for analyzer_type, analyzer_func in analyzers:
-        result, error = analyzer_func(
-            connection, step) if analyzer_type == 'semant' else analyzer_func(step)
+        result, error = (
+            analyzer_func(connection, step)
+            if analyzer_type == "semant"
+            else analyzer_func(step)
+        )
 
         if error:
             for a, _ in analyzers:
                 if flag:
-                    results[f'{a}_result'] = ""
+                    results[f"{a}_result"] = ""
                 if analyzer_type == a:
-                    results[f'{analyzer_type}_result'] = result
+                    results[f"{analyzer_type}_result"] = result
                     flag = True
             session.update(results)
             return_result.append(
-                f'Hubo un error {analyzer_type}ico, revisa las salidas de los analizadores.')
+                f"Hubo un error {analyzer_type}ico, revisa las salidas de los analizadores."
+            )
             return return_result
 
-        results[f'{analyzer_type}_result'] = result
+        results[f"{analyzer_type}_result"] = result
 
     session.update(results)
     return_result.append(execute_query(connection, step))
@@ -82,22 +82,22 @@ def flow(connection, *steps):
     return results
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def front():
-    analyzers = ['lex', 'sintact', 'semant']
+    analyzers = ["lex", "sintact", "semant"]
     results = {}
     s = []
 
-    lex_result = session.get('lex_result', '')
-    sintact_result = session.get('sintact_result', '')
-    semant_result = session.get('semant_result', '')
+    lex_result = session.get("lex_result", "")
+    sintact_result = session.get("sintact_result", "")
+    semant_result = session.get("semant_result", "")
 
-    host = 'localhost'
-    user = 'root'
-    password = 'Jetcrab333'
+    host = "localhost"
+    user = "root"
+    password = "Jetcrab333"
     connection = create_mysql_connection(host, user, password)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         steps = [
             request.form.get("step_1", ""),
             request.form.get("step_2", ""),
@@ -107,15 +107,40 @@ def front():
         ]
 
         button_areas = [
-            ("btn_1", "area_1", run_lexical_analyzer,
-             "Análisis léxico ejecutado", "lex_result", None),
-            ("btn_2", "area_2", run_syntax_analyzer,
-             "Análisis sintáctico ejecutado", "sintact_result", None),
-            ("btn_3", "area_3", run_semantic_analyzer,
-             "Análisis semántico ejecutado", "semant_result", "connection"),
+            (
+                "btn_1",
+                "area_1",
+                run_lexical_analyzer,
+                "Análisis léxico ejecutado",
+                "lex_result",
+                None,
+            ),
+            (
+                "btn_2",
+                "area_2",
+                run_syntax_analyzer,
+                "Análisis sintáctico ejecutado",
+                "sintact_result",
+                None,
+            ),
+            (
+                "btn_3",
+                "area_3",
+                run_semantic_analyzer,
+                "Análisis semántico ejecutado",
+                "semant_result",
+                "connection",
+            ),
         ]
 
-        for button, area, analyzer_func, message, result_key, additional_param in button_areas:
+        for (
+            button,
+            area,
+            analyzer_func,
+            message,
+            result_key,
+            additional_param,
+        ) in button_areas:
             if request.form.get(button):
                 x = request.form.get(area)
                 if x:
@@ -123,8 +148,11 @@ def front():
                         a, b = analyzer_func(additional_param, x)
                     else:
                         a, b = analyzer_func(x)
-                    results = {'lex_result': "",
-                               'sintact_result': "", 'semant_result': ""}
+                    results = {
+                        "lex_result": "",
+                        "sintact_result": "",
+                        "semant_result": "",
+                    }
                     results[result_key] = a
                     session.update(results)
                 flash(message, "notify")
@@ -135,27 +163,29 @@ def front():
             if steps[1]:
                 result = flow(connection, *steps[1:])
             print(result)
-            flash(result[-1], 'notify')
+            flash(result[-1], "notify")
         elif steps[1]:
             result = flow(connection, *steps[1:])
-            flash(result[-1], 'notify')
+            flash(result[-1], "notify")
         elif steps[2]:
             result = flow(connection, *steps[2:])
-            flash(result[-1], 'notify')
+            flash(result[-1], "notify")
         elif steps[3]:
             result = flow(connection, *steps[3:])
-            flash(result[-1], 'notify')
+            flash(result[-1], "notify")
         elif steps[4]:
             result = logic(connection, steps[4])
-            flash(results[-1], 'notify')
+            flash(results[-1], "notify")
         else:
             for n in analyzers:
-                results[f'{n}_result'] = ""
+                results[f"{n}_result"] = ""
             session.update(results)
-            flash('Error, revisa los campos faltantes o introducidos.', 'notify')
-        return redirect(url_for('front'))
+            flash("Error, revisa los campos faltantes o introducidos.", "notify")
+        return redirect(url_for("front"))
 
-    return render_template('front.html', value_1=lex_result, value_2=sintact_result, value_3=semant_result)
+    return render_template(
+        "front.html", value_1=lex_result, value_2=sintact_result, value_3=semant_result
+    )
 
 
 def main():
